2b6d0a4ffefa7c062dea1708fbc4591ab7012caf
lzd <234687552@qq.omc>
2017-11-20 18:00:56 +0800

    更改jdbc驱动使jpa打印出参数

--- phantom-service-provider/src/main/java/com/kingboy/repository/user/UserRepository.java
+++ phantom-service-provider/src/main/java/com/kingboy/repository/user/UserRepository.java
@@ -4,6 +4,8 @@ import com.kingboy.repository.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author kingboy--KingBoyWorld@163.com
  * @date 2017/11/19 下午10:40
@@ -11,5 +13,15 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    List<UserEntity> findAllByName(String name);
+
+    UserEntity findByName(String name);
+
+    int countByName(String name);
+
+    UserEntity readByName(String name);
+
+    UserEntity getByName(String name);
+
 
 }
--- phantom-service-provider/src/test/java/com/kingboy/PhantomServiceProviderApplicationTests.java
+++ phantom-service-provider/src/test/java/com/kingboy/PhantomServiceProviderApplicationTests.java
@@ -1,16 +1,38 @@
 package com.kingboy;
 
+import com.kingboy.repository.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class PhantomServiceProviderApplicationTests {
 
+    @Resource
+    UserRepository userRepository;
     @Test
     public void contextLoads() {
     }
+
+    @Test
+    public void  testRepository(){
+        log.info("countByName");
+        userRepository.countByName("a");
+        log.info("readByName");
+        userRepository.readByName("a");
+        log.info("findByName");
+        userRepository.findByName("a");
+        log.info("getByName");
+        userRepository.getByName("a");
+        log.info("findAllByName");
+        userRepository.findAllByName("a");
+    }
+
 
 }
